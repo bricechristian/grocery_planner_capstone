957a6f69a6c66b8547f24bb563d3bf361ccc1a57
Brice Christian <bricechristian93@gmail.com>
2017-03-13 00:53:36 -0400

    Added a few things to index and server

--- server.js
+++ server.js
@@ -1,12 +1,35 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
 const app = express();
 const {groceryList} = require('./models')
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended:true }));
+app.use(bodyParser.json());
 
+
+mongoose.connect('mongodb://bricechristian:brice2006@ds129090.mlab.com:29090/grocery-list')
 
 
 app.get('/', (req, res) => {
 	res.sendFile('index.html');
+})
+
+app.get('/', (req, res) => {
+	groceryList
+	.find()
+})
+
+app.post('/', (req, res) => {
+
+})
+
+app.put('/', (req, res) => {
+
+})
+
+app.delete('/', (req, res) => {
+
 })
 
 
